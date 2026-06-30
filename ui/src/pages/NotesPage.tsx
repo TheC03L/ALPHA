d3e45f56a89d288e5ac9f27030a609dfd6aa9f55
vector <abdullah.harmonytx@gmail.com>
2026-06-30 08:39:05 -0500

    Add missing AlertTriangle import in NotesPage.tsx

--- ui/src/pages/NotesPage.tsx
+++ ui/src/pages/NotesPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react'
 import {
   FileText, Plus, X, Trash2, Loader, AlertCircle, Check,
   Info, Search, Edit3, Save, Clock
-} from 'lucide-react'
+, AlertTriangle} from 'lucide-react'
 import api from '../utils/api'
 
 interface Note {
