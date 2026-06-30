55559942c0b057bed710a97c93661ff368020375
vector <abdullah.harmonytx@gmail.com>
2026-06-30 08:40:16 -0500

    Fix AlertTriangle import formatting in FavoritesPage.tsx

--- ui/src/pages/FavoritesPage.tsx
+++ ui/src/pages/FavoritesPage.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useCallback } from 'react'
 import {
   Star, File, FileText, Image, Music, Video, Archive,
   Trash2, Loader, AlertCircle, Check, Info, X
-, AlertTriangle} from 'lucide-react'
+, AlertTriangle
+} from 'lucide-react'
 import api from '../utils/api'
 
 interface FavoriteItem {
